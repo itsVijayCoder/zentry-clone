a1ace70b7965ab96facb50b3ba6586e9004c901f
tomvj1258 <tomvj15@gmail.com>
2025-01-21 17:37:22 +0530

    [added]: Features section with BentoCard & BentoTilt

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import About from "./components/About.jsx";
+import Features from "./components/Features.jsx";
 import GridDemo from "./components/grid-demo.jsx";
 import Hero from "./components/Hero.jsx";
 import Navbar from "./components/Navbar.jsx";
 
 const App = () => {
    return (
-      <main className='relative min-h-screen w-screen overflow-x-hidden '>
+      <main className='relative min-h-screen w-screen overflow-x-hidden overflow-hidden '>
          <Navbar />
          <Hero />
          <About />
+         <Features />
 
-         {/* <GridDemo /> */}
          {/* <section className='min-h-screen bg-blue-75 z-0' /> */}
       </main>
    );
